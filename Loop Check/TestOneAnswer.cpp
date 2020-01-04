d8fa7ed4a7de1e05babe59194a1b2ac5e91b87dd
Duygu Bayram <43537425+velnxn@users.noreply.github.com>
2020-01-04 12:03:52 +0100

    Update TestOneAnswer.cpp

--- Loop Check/TestOneAnswer.cpp
+++ Loop Check/TestOneAnswer.cpp
@@ -7,9 +7,7 @@ int main(){
   std::cout << "Please enter a number: " << std::endl;
   std::cin >> num;
 
-  //Very close, but this does not display anything.
-  //Hint: What number is i?
-  for(int i; i < num; i += 2) {
+  for(int i == 0; i < num; i += 2) {
     std::cout << "Even numbers up to entered number: "<< i << std::endl;
   }
 
