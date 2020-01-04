ba718d792854acc5ca28573122287837ec8256d0
NicholasReis <46433505+NicholasReis@users.noreply.github.com>
2020-01-04 02:46:29 -0800

    Close, but needs 2 more characters.

--- Loop Check/TestOneAnswer.cpp
+++ Loop Check/TestOneAnswer.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 
 int main(){
-    
+
   int num;
 
   std::cout << "Please enter a number: " << std::endl;
   std::cin >> num;
 
-    for(int i; i < num; i += 2) {
-        std::cout << "Even numbers up to entered number: "<< i << std::endl;
-        }
-        
+  //Very close, but this does not display anything.
+  //Hint: What number is i?
+  for(int i; i < num; i += 2) {
+    std::cout << "Even numbers up to entered number: "<< i << std::endl;
+  }
+
   return 0;
 }
